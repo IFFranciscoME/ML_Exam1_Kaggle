f751df6a7b9d609e614d75f40389d6c6dacf45e7
Francisco Muñoz <if.francisco.me@gmail.com>
2021-03-19 20:29:12 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+# --------------------------------------------------------------------------------------------------------- #
+# -- project: Name of the Kaggle Competition                                                             -- #
+# -- File: main.py | Main functionallity code                                                            -- #
+# -- author: Name of the Team or the Author                                                              -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- repository/notebook: Private repository URL and/or public notebook in kaggle                        -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
